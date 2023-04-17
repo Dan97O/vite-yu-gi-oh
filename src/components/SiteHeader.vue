f85523e870acc08e4fec982ff75cfcfe81f4290a
Dan97O <dan97-oddo@live.it>
2023-04-17 15:11:27 +0200

    add SiteHeader

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -5,7 +5,16 @@ export default {
 </script>
 
 
-<template></template>
+<template>
+  <div class="py-3 d-flex align-items-center">
+    <img class="img-fluid px-3" src="https://upload.wikimedia.org/wikipedia/fr/a/a5/Yu-Gi-Oh_Logo.JPG" alt="">
+    <h1 class="m-0">Yu-Gi-Oh Api</h1>
+  </div>
+</template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.img-fluid {
+  height: 20px;
+}
+</style>
