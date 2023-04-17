5719b5d8917e651826cbad122eaa955c89dd6648
Dan97O <dan97-oddo@live.it>
2023-04-17 14:46:43 +0200

    add created element SiteHeader.vue / SiteMain.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script>
+import SiteHeader from './components/SiteHeader.vue'
+import SiteMain from './components/SiteMain.vue'
 
+export default {
+  components: {
+    SiteHeader,
+    SiteMain,
+  },
+  data() {
+    return {
+
+    }
+  },
+
+}
 </script>
 
-<template></template>
+<template>
+  <SiteHeader />
+  <SiteMain />
+</template>
 
-<style ></style>
+<style></style>
